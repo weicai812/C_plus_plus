744fea49513776f6dd032f0bf4f80dffaaea4d75
GAN WEI CAI <60889205+weicai812@users.noreply.github.com>
2021-02-18 15:33:58 +0800

    Create Fundamental.cpp

---
+++ Fundamental.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x =12;
+    double y=33.98;
+    cout << y <<"\n";
+    cout << x << "\n";
+    cout << "Hello, world!";
+    return 0;
+}
